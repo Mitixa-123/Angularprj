dd877ded4714c1f89b909e3082b10672538ba0d6
Mitixa <vaghasiyamitixa28@gmail.com>
2025-05-04 21:46:49 +0530

    all events demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angularprj';
-  btn:string ="click me"
+  btn:string ="click me";
+  count: number = 0;
+  inputValue = '';
+  focusStatus = '';
+  mouseStatus = '';
+  key = '';
+  windowWidth = window.innerWidth;
+  droppedData = '';
   // attr binding
 
   isDisabled:boolean = true;
@@ -15,4 +22,83 @@ export class AppComponent {
   
   //style binding
   backcolor:string = "text-size:40px; backgroundColor:yellow";
+
+  //class Binding
+
+  isTextRed:string = "yes";
+
+  // function 
+
+
+
+  incrementNumber() {
+    this.count++;
+  }
+
+  OnInputChange(event: Event){
+    const target = event.target as HTMLInputElement;
+    this.inputValue = target.value;
+  }
+  onDoubleClick() {
+    alert('Double-click detected!');
+  }
+
+  onMouseDown() {
+    this.mouseStatus = 'Mouse button down';
+  }
+
+  onMouseUp() {
+    this.mouseStatus = 'Mouse button released';
+  }
+
+  onMouseEnter() {
+    this.mouseStatus = 'Mouse entered element';
+  }
+
+  onMouseLeave() {
+    this.mouseStatus = 'Mouse left element';
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    this.key = `Key down: ${event.key}`;
+  }
+  onKeyUp(event: KeyboardEvent) {
+    this.key = `Key Up: ${event.key}`;
+  }
+
+  onChange(event: Event) {
+    alert('Input changed and focus lost');
+  }
+
+  onFocus() {
+    this.focusStatus = 'Input is focused';
+  }
+
+  onBlur() {
+    this.focusStatus = 'Input lost focus';
+  }
+
+  onScroll(event: Event) {
+    console.log('Scrolled!', event);
+  }
+
+  // @HostListener('window:resize', ['$event'])
+  // onResize(event: UIEvent) {
+  //   this.windowWidth = window.innerWidth;
+  // }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.droppedData = event.dataTransfer?.getData('text') || '';
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  onDragStart(event: DragEvent) {
+    event.dataTransfer?.setData('text', 'This is draggable content');
+  }
+
+  
 }
